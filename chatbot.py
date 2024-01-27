13522d2a0f4d92183e11bdf07c666597eb12e032
Byung Chun Kim <wizardbc@gmail.com>
2024-01-27 22:26:07 +0900

    using

--- chatbot.py
+++ chatbot.py
@@ -1,38 +1,79 @@
 import streamlit as st
+import google.generativeai as genai
 
-st.title("💬 Echo bot")
-st.caption("🚀 A streamlit echo bot")
+st.title("💬 Chatbot")
+st.caption("🚀 A streamlit chatbot powered by Google Gemini-Pro")
+
+# Google API key
+if "api_key" not in st.session_state:
+  try:
+    st.session_state.api_key = st.secrets["GOOGLE_API_KEY"]
+  except:
+    st.session_state.api_key = ""
+    st.write("Your Google API Key is not provided in `.streamlit/secrets.toml`, but you can input one in the sidebar for temporary use.")
 
 # Initialize chat history
 if "messages" not in st.session_state:
   st.session_state.messages = []
 
+# Sidebar for parameters
+with st.sidebar:
+  # Google API Key
+  if not st.session_state.api_key:
+    st.header("Google API Key")
+    st.session_state.api_key = st.text_input("Google API Key", type="password")
+  else:
+    genai.configure(api_key=st.session_state.api_key)
+
+  # ChatCompletion parameters
+  st.header("Parameters")
+  model_name = st.selectbox("model_name",
+      ['gemini-pro'])
+  
+  generation_config = {
+    "temperature": st.slider("temperature", min_value=0.0, max_value=1.0, value=0.9),
+    "max_output_tokens": st.number_input("max_tokens", min_value=1, value=2048),
+    "top_k": st.slider("top_k", min_value=1, value=32),
+    "top_p": st.slider("top_p", min_value=0.0, max_value=1.0, value=1.0),
+  }
+
 # Display messages in history
 for msg in st.session_state.messages:
-  if content := msg.get("content", ""):
-    with st.chat_message(msg.get("role")):
-      st.write(content)
+  if parts := msg.get("parts", []):
+    with st.chat_message('human' if msg.get("role") == 'user' else 'ai'):
+      for p in parts:
+        st.write(p)
 
 # Chat input
 if prompt := st.chat_input("What is up?"):
   # User message
   user_msg = {
-    "role": "user",
-    "content": prompt,
+    "role": 'user',
+    "parts": [prompt]
   }
+  
   # Display user message
-  with st.chat_message("user"):
+  with st.chat_message('human'):
     st.write(prompt)
   # Append to history
   st.session_state.messages.append(user_msg)
 
-  # Assistant message
-  assistant_msg = {
-    "role": "assistant",
-    "content": f"(ECHO) {prompt}"
-  }
-  # Display assistant message
-  with st.chat_message("assistant"):
-    st.write(assistant_msg.get("content"))
+  # Generate
+  model = genai.GenerativeModel(model_name=model_name,
+                                generation_config=generation_config)
+  response = model.generate_content(st.session_state.messages, stream=True)
+
+  # Stream display
+  with st.chat_message("ai"):
+    placeholder = st.empty()
+  text = ''
+  for chunk in response:
+    text += chunk.text
+    placeholder.write(text + "▌")
+  placeholder.write(text)
+
   # Append to history
-  st.session_state.messages.append(assistant_msg)
+  st.session_state.messages.append({
+    'role': 'model',
+    'parts': [text]
+  })
